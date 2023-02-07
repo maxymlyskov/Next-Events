a8c0ed619242ebde02046839061d7dbf0ce400d3
maxymlyskov <raypember02@gmail.com>
2023-02-07 15:58:36 +0200

    Added link navigation.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>hello world</h1>
+      <ul>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/portfolio/21">Profolio 21</Link>
+        </li>
+      </ul>
     </>
   );
 }
