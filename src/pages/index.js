5cb4ea5e03c61a55bbd0e103e021533879df3e07
maxymlyskov <raypember02@gmail.com>
2023-02-07 22:40:47 +0200

    Initial commit.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
-  const clients = [
-    { id: "first", name: "First" },
-    { id: "second", name: "Second" },
-  ];
-  return (
-    <>
-      <ul>
-        {clients.map((client) => (
-          <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <></>;
 }
