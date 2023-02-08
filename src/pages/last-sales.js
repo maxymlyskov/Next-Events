587a7b8c0e9f432913b79f34afc2d592a4cf08bf
maxymlyskov <raypember02@gmail.com>
2023-02-08 21:40:31 +0200

    Customized last slaed with useSWR

--- src/pages/last-sales.js
+++ src/pages/last-sales.js
@@ -1,27 +1,50 @@
 import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function LastSales() {
   const [sales, setSales] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  //   const [isLoading, setIsLoading] = useState(true);
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-be10b-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
-    fetch("https://nextjs-course-be10b-default-rtdb.firebaseio.com/sales.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
+    if (data) {
+      const transformedSales = [];
 
-  if (isLoading) return <p>Loading...</p>;
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+  //   useEffect(() => {
+  //     fetch("https://nextjs-course-be10b-default-rtdb.firebaseio.com/sales.json")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) return <p>Failed to load.</p>;
+
+  if (!data || !sales) return <p>Loading...</p>;
 
   return (
     <ul>
