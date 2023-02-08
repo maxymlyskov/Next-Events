1dd93330cc4077df8e6666703806f92157db249f
maxymlyskov <raypember02@gmail.com>
2023-02-08 12:56:49 +0200

    Added navigating filters

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,13 +1,21 @@
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
 import { getAllEvents, getFeaturedEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function AllEventsPage() {
+  const router = useRouter();
   const allEvents = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={allEvents} />
     </div>
   );
